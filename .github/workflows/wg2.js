8d5eadf2c6e2ffde9e617f335b71813791744966
Ahmad Alzhrani <alzhrani.ahmadf@gmail.com>
2024-02-06 14:04:44 +0300

    try my action

--- .github/workflows/wg2.js
+++
@@ -1,9 +0,0 @@
-var exec = require('child_process').exec;
-
-exec('npx write-good *.md',
-    function (error, stdout, stderr) {
-        console.log( stdout);
-        if (error !== null) {
-             console.log('exec: write-good');
-        }
-    });
